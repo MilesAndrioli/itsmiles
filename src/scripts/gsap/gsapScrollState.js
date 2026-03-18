c4db5ab1cde145f35b831a2123fff3675b52985e
Miles <98649838+MilesAndrioli@users.noreply.github.com>
2026-03-18 01:11:27 +0100

    Detected the issue, not running gsapScrollState anymore, will handle scroll progress in the component itself.

--- src/scripts/gsap/gsapScrollState.js
+++ src/scripts/gsap/gsapScrollState.js
@@ -5,59 +5,60 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default function initGsapScrollState() {
     const root = document.documentElement;
-    // const main = document.getElementById("app-main");
-    // const footer = document.getElementById("app-footer");
+    const main = document.getElementById("app-main");
+    const footer = document.getElementById("app-footer");
 
     // --- Milestone 1 ---
-    // ScrollTrigger.create({
-    //     trigger: main,
-    //     start: "top top",
-    //     toggleClass: {
-    //         targets: root,
-    //         className: "HAS-REACHED--STEP-1",
-    //     },
-    //     markers: false,
-    // });
+    ScrollTrigger.create({
+        trigger: main,
+        start: "top top",
+        toggleClass: {
+            targets: root,
+            className: "HAS-REACHED--STEP-1",
+        },
+        markers: false,
+    });
 
     // --- Milestone 2 ---
-    // ScrollTrigger.create({
-    //     trigger: root,
-    //     start: `${innerHeight} top`,
-    //     toggleClass: {
-    //         targets: root,
-    //         className: "HAS-REACHED--STEP-2",
-    //     },
-    //     markers: false,
-    // });
+    ScrollTrigger.create({
+        trigger: root,
+        start: `${innerHeight} top`,
+        toggleClass: {
+            targets: root,
+            className: "HAS-REACHED--STEP-2",
+        },
+        markers: false,
+    });
 
     // --- Milestone Footer ---
-    // ScrollTrigger.create({
-    //     trigger: footer,
-    //     start: "top bottom",
-    //     toggleClass: {
-    //         targets: root,
-    //         className: "HAS-REACHED--FOOTER",
-    //     },
-    //     markers: false,
-    // });
+    ScrollTrigger.create({
+        trigger: footer,
+        start: "top bottom",
+        toggleClass: {
+            targets: root,
+            className: "HAS-REACHED--FOOTER",
+        },
+        markers: false,
+    });
 
     // --- Scroll Direction ---
-    // let lastDirection = 0;
+    let lastDirection = 0;
 
-    // ScrollTrigger.create({
-    //     onUpdate: (self) => {
-    //         if (self.direction !== lastDirection) {
-    //             root.classList.toggle("SCROLLING--DOWN", self.direction === 1);
-    //             root.classList.toggle("SCROLLING--UP", self.direction === -1);
-    //             lastDirection = self.direction;
-    //         }
-    //     },
-    // });
+    ScrollTrigger.create({
+        onUpdate: (self) => {
+            if (self.direction !== lastDirection) {
+                root.classList.toggle("SCROLLING--DOWN", self.direction === 1);
+                root.classList.toggle("SCROLLING--UP", self.direction === -1);
+                lastDirection = self.direction;
+            }
+        },
+    });
 
     // --- Scroll Progress ---
-    if (window.__LENIS__) {
-        window.__LENIS__.on("scroll", ({ progress }) => {
-            root.style.setProperty("--SCROLL_PROGRESS", progress.toFixed(4));
-        });
-    }
+    // BECAREFUL: HUGE PERFORMANCE IMPACT, REVISIT
+    // if (window.__LENIS__) {
+    //     window.__LENIS__.on("scroll", ({ progress }) => {
+    //         root.style.setProperty("--SCROLL_PROGRESS", progress.toFixed(4));
+    //     });
+    // }
 }
