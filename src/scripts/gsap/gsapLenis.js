148b7c46471576e6a94df00fe66b26d6dfc3e856
Miles <98649838+MilesAndrioli@users.noreply.github.com>
2026-04-02 05:05:54 +0200

    REVISIT playing with Lenis settings

--- src/scripts/gsap/gsapLenis.js
+++ src/scripts/gsap/gsapLenis.js
@@ -6,11 +6,27 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
+// --- PRESET 0 ---
+// const LENIS_OPTIONS = {
+//     lerp: 0.08,
+//     lerp: 0.05, // more friction/heavier momentum
+//     wheelMultiplier: 0.9,
+// };
+
+// --- HEAVY CINEMATIC + ---
+// const LENIS_OPTIONS = {
+//     duration: 1.4,
+//     easing: (t) => 1 - Math.pow(1 - t, 4), // easeOutQuart
+//     wheelMultiplier: 0.95,
+// };
+
+// --- HEAVY CINEMATIC - ---
+const LENIS_OPTIONS = {
+    easing: (t) => 1 - Math.pow(1 - t, 3), // easeOutCubic
+};
+
 export default function initGsapLenis() {
-    window.__LENIS__ = new Lenis({
-        lerp: 0.08,
-        wheelMultiplier: 0.9,
-    });
+    window.__LENIS__ = new Lenis(LENIS_OPTIONS);
 
     // --- GSAP x Lenis Integration ---
     window.__LENIS__.on("scroll", ScrollTrigger.update);
