917282805443435d4b3d50d167500c0031cafb1d
Miles <98649838+MilesAndrioli@users.noreply.github.com>
2026-04-10 23:37:56 +0200

    new font loading system

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import yaml from "@rollup/plugin-yaml";
 import astroExpressiveCode from "astro-expressive-code";
@@ -12,6 +12,69 @@ export default defineConfig({
     vite: {
         plugins: [tailwindcss(), yaml()],
     },
+    fonts: [
+        {
+            provider: fontProviders.local(),
+            name: "Montreal",
+            cssVariable: "--font-montreal",
+            options: {
+                variants: [
+                    {
+                        src: ["./src/assets/fonts/Montreal.woff2"],
+                        weight: "normal",
+                        style: "normal",
+                    },
+                ],
+            },
+        },
+        {
+            provider: fontProviders.local(),
+            name: "Gatwick",
+            cssVariable: "--font-gatwick",
+            options: {
+                variants: [
+                    {
+                        src: ["./src/assets/fonts/Gatwick.woff2"],
+                        weight: "200",
+                        style: "normal",
+                    },
+                ],
+            },
+        },
+        {
+            provider: fontProviders.local(),
+            name: "Monument",
+            cssVariable: "--font-monument",
+            options: {
+                variants: [
+                    {
+                        src: ["./src/assets/fonts/Monument.woff2"],
+                        weight: "normal",
+                        style: "normal",
+                    },
+                    {
+                        src: ["./src/assets/fonts/Monument-Light.woff2"],
+                        weight: "300",
+                        style: "normal",
+                    },
+                ],
+            },
+        },
+        {
+            provider: fontProviders.local(),
+            name: "Lisa",
+            cssVariable: "--font-lisa",
+            options: {
+                variants: [
+                    {
+                        src: ["./src/assets/fonts/Lisa.woff2"],
+                        weight: "normal",
+                        style: "normal",
+                    },
+                ],
+            },
+        },
+    ],
     integrations: [astroExpressiveCode(), mdx()],
     markdown: {
         rehypePlugins: [
