a71da3cc0edc8138142026da2cffcfe1e646fd1c
Miles <98649838+MilesAndrioli@users.noreply.github.com>
2026-04-18 08:53:30 +0200

    Update gsapConfig.js

--- src/scripts/gsapConfig.js
+++ src/scripts/gsapConfig.js
@@ -1,7 +1,9 @@
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { CustomEase } from "gsap/CustomEase";
+import { SplitText } from "gsap/SplitText";
 
-gsap.registerPlugin(CustomEase);
+gsap.registerPlugin(ScrollTrigger, CustomEase, SplitText);
 
 export default function gsapConfig() {
     gsap.defaults({
